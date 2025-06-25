c4a817a4cf9f9be6b627af064cceac9c5ace32e5
Michael Hatton <michael@mavericks-vfx.com>
2025-06-25 14:04:10 -0400

    add better surround commands

--- init.lua
+++ init.lua
@@ -154,7 +154,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 3
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -309,7 +309,7 @@ require('lazy').setup({
         { '<leader>a', group = '[A]I CodeCompanion', mode = { 'n', 'v' } },
         { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
         { '<leader>d', group = '[D]ocument' },
-        { '<leader>g', group = 'Hi[G]hLight', mode = { 'n', 'x'} },
+        { '<leader>g', group = 'Hi[G]hLight', mode = { 'n', 'x' } },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
@@ -1017,7 +1017,21 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'csa', -- Add surrounding in Normal and Visual modes
+          delete = 'ds', -- Delete surrounding
+          -- delete = 'csd', -- Delete surrounding
+          find = 'csf', -- Find surrounding (to the right)
+          find_left = 'csF', -- Find surrounding (to the left)
+          highlight = 'csh', -- Highlight surrounding
+          replace = 'csr', -- Replace surrounding
+          update_n_lines = 'csn', -- Update `n_lines`
+
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+      }
 
       require('mini.pairs').setup()
       -- Simple and easy statusline.
@@ -1294,8 +1308,6 @@ require('lazy').setup({
       -- vim.keymap.set('n', 'gl', '<cmd>Hi [<CR>', { desc = 'Hi[g]hlight jump back any hl' })
       -- vim.keymap.set('n', '<leader>gs', '<cmd>HighlightedyankSetSL<CR>', { desc = 'Highlightedyank SetSL' })
     end,
-
-
   },
   {
     'preservim/tagbar',
@@ -1406,6 +1418,105 @@ require('lazy').setup({
 
     event = { 'BufReadPre', 'BufNewFile' },
   },
+  -- {
+  --   'Vigemus/iron.nvim',
+  --   opts = {},
+  --   -- config = function() end,
+  --   config = function()
+  --     local iron = require 'iron.core'
+  --     local view = require 'iron.view'
+  --     local common = require 'iron.fts.common'
+  --
+  --     iron.setup {
+  --       config = {
+  --         -- Whether a repl should be discarded or not
+  --         scratch_repl = true,
+  --         -- Your repl definitions come here
+  --         repl_definition = {
+  --           sh = {
+  --             -- Can be a table or a function that
+  --             -- returns a table (see below)
+  --             command = { 'zsh' },
+  --           },
+  --           python = {
+  --             command = { 'python3' }, -- or { "ipython", "--no-autoindent" }
+  --             format = common.bracketed_paste_python,
+  --             block_dividers = { '# %%', '#%%' },
+  --           },
+  --         },
+  --         -- set the file type of the newly created repl to ft
+  --         -- bufnr is the buffer id of the REPL and ft is the filetype of the
+  --         -- language being used for the REPL.
+  --         repl_filetype = function(bufnr, ft)
+  --           return ft
+  --           -- or return a string name such as the following
+  --           -- return "iron"
+  --         end,
+  --         -- How the repl window will be displayed
+  --         -- See below for more information
+  --         repl_open_cmd = view.bottom(40),
+  --
+  --         -- repl_open_cmd can also be an array-style table so that multiple
+  --         -- repl_open_commands can be given.
+  --         -- When repl_open_cmd is given as a table, the first command given will
+  --         -- be the command that `IronRepl` initially toggles.
+  --         -- Moreover, when repl_open_cmd is a table, each key will automatically
+  --         -- be available as a keymap (see `keymaps` below) with the names
+  --         -- toggle_repl_with_cmd_1, ..., toggle_repl_with_cmd_k
+  --         -- For example,
+  --         --
+  --         -- repl_open_cmd = {
+  --         --   view.split.vertical.rightbelow("%40"), -- cmd_1: open a repl to the right
+  --         --   view.split.rightbelow("%25")  -- cmd_2: open a repl below
+  --         -- }
+  --       },
+  --       -- Iron doesn't set keymaps by default anymore.
+  --       -- You can set them here or manually add keymaps to the functions in iron.core
+  --       keymaps = {
+  --         toggle_repl = '<space>rr', -- toggles the repl open and closed.
+  --         -- If repl_open_command is a table as above, then the following keymaps are
+  --         -- available
+  --         -- toggle_repl_with_cmd_1 = "<space>rv",
+  --         -- toggle_repl_with_cmd_2 = "<space>rh",
+  --         restart_repl = '<space>rR', -- calls `IronRestart` to restart the repl
+  --         send_motion = '<space>sc',
+  --         visual_send = '<space>sc',
+  --         send_file = '<space>sf',
+  --         send_line = '<space>sl',
+  --         send_paragraph = '<space>sp',
+  --         send_until_cursor = '<space>su',
+  --         send_mark = '<space>sm',
+  --         send_code_block = '<space>sb',
+  --         send_code_block_and_move = '<space>sn',
+  --         mark_motion = '<space>mc',
+  --         mark_visual = '<space>mc',
+  --         remove_mark = '<space>md',
+  --         cr = '<space>s<cr>',
+  --         interrupt = '<space>s<space>',
+  --         exit = '<space>sq',
+  --         clear = '<space>cl',
+  --       },
+  --       -- If the highlight is on, you can change how it looks
+  --       -- For the available options, check nvim_set_hl
+  --       highlight = {
+  --         italic = true,
+  --       },
+  --       ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+  --     }
+  --
+  --     -- iron also has a list of commands, see :h iron-commands for all available commands
+  --     vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
+  --     vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+  --     --
+  --   end,
+  -- },
+
+  -- {
+  --   'bfredl/nvim-ipy',
+  --   opts = {},
+  --   config = function() end,
+  -- },
+
 
   --- put plugins before this...
 }, {
